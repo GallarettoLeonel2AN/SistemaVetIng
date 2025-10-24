d5a4e4d1899578e1c9917af8829b161afdc96a23
SosaUlises <ulipvp@gmail.com>
2025-10-24 16:53:09 -0300

    Ajustes en css y cshtml

--- SistemaVetIng/obj/Debug/net8.0/SistemaVetIng.AssemblyInfo.cs
+++ SistemaVetIng/obj/Debug/net8.0/SistemaVetIng.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b3a6636ff3dbefb9dc39fd88d980c39df5bba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe79eb95186737b5c5baab6d10274d56ef51c2d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
