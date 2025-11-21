0d53f9bdf778549c866dbf8a9087a49fb70de249
SosaUlises <ulipvp@gmail.com>
2025-11-21 13:04:42 -0300

    Cambios en gestion de roles por usuario

--- SistemaVetIng/obj/Debug/net8.0/SistemaVetIng.AssemblyInfo.cs
+++ SistemaVetIng/obj/Debug/net8.0/SistemaVetIng.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ef96fe9504042d1d0d61d703fce811d445b559")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372b49b25b819dd0f5e3ac3dcf7b065b76c6d5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
